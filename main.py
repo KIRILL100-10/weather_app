bf4eb7936c8104fe63b7bc7ea0a0aa24d45ddc39
Kirill Nadtochaev <Kirillnadtocaev@gmail.com>
2026-02-27 19:05:26 +0300

    New update

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 import requests
 
-def get_weather(city):
-    api_key = 'your_api_key'
+def get_weather(city, api_key):
     url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={api_key}'
     try:
         response = requests.get(url)
         response_json = response.json()
         if response.status_code == 200:
-            with open('weather.txt', 'w', encoding='utf-8') as file:
-                file.write(f'City: {response_json['name']}\n')
-                file.write(f'Temperature: {response_json['main']['temp']}°C\n')
-                file.write(f'Description: {response_json['weather'][0]['description'].capitalize()}\n')
-                file.write(f'Humidity: {response_json['main']['humidity']}%\n')
-                file.write(f'Wind speed: {response_json['wind']['speed']} m/s\n')
-                file.write(f'Feels like: {response_json['main']['feels_like']}°C\n')
-                file.write(f'Pressure: {response_json['main']['pressure']} hPa\n')
+            print(f'City: {response_json['name']}')
+            print(f'Temperature: {response_json['main']['temp']}°C')
+            print(f'Description: {response_json['weather'][0]['description'].capitalize()}')
+            print(f'Humidity: {response_json['main']['humidity']}%')
+            print(f'Wind speed: {response_json['wind']['speed']} m/s')
+            print(f'Feels like: {response_json['main']['feels_like']}°C')
+            print(f'Pressure: {response_json['main']['pressure']} hPa')
         else:
             return 'Error!'
     except requests.exceptions.RequestException as e:
@@ -23,9 +21,9 @@ def get_weather(city):
         return 'Unexpected API response. Please check your inputs'
     except ValueError:
         return 'Please enter a valid city'
-
 try:
+    api_key = input('Enter an API key: ')
     city = input('Enter a city: ')
-    get_weather(city=city)
+    get_weather(city=city, api_key=api_key)
 except ValueError:
     print('Invalid input. Please enter a numeric value for the city')
